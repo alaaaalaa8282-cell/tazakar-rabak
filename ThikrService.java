8496b994c6bed61400228cef54bb836f1b39bfcd
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-02 21:09:08 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- ThikrService.java
+++ ThikrService.java
@@ -110,6 +110,7 @@ public class ThikrService extends IntentService  {
         //update all alarms
         Intent boot_reciever = new Intent("com.alaaeltaweel.thikrallah.Notification.ThikrBootReceiver.android.action.broadcast");
         this.sendBroadcast(boot_reciever);
+		}
         Log.d(TAG,"onhandleintnet called");
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
         String lang=sharedPrefs.getString("language",null);
