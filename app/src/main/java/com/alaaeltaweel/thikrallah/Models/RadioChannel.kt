2899b4ef32fc54f0c30b0fb3f999f0aa0f170074
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-23 22:33:31 +0300

    Update RadioChannel.kt

--- app/src/main/java/com/alaaeltaweel/thikrallah/Models/RadioChannel.kt
+++ app/src/main/java/com/alaaeltaweel/thikrallah/Models/RadioChannel.kt
@@ -1,1 +1,8 @@
-RadioChannel.kt
+package com.alaaeltaweel.thikrallah.domain.entity.radio
+
+data class RadioChannel(
+    val id: Int,
+    val nameAr: String,
+    val nameEn: String,
+    val streamUrl: String
+)
