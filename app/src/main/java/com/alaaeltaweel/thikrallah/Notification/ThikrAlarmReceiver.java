6b663c7c67f0d24d5d3ced69eeadfa8db4667b00
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-06-25 11:57:21 +0300

    Refactor ThikrAlarmReceiver for better clarity

--- app/src/main/java/com/alaaeltaweel/thikrallah/Notification/ThikrAlarmReceiver.java
+++ app/src/main/java/com/alaaeltaweel/thikrallah/Notification/ThikrAlarmReceiver.java
@@ -35,7 +35,7 @@ import com.alaaeltaweel.thikrallah.MainActivity;
 
 import com.alaaeltaweel.thikrallah.R;
 
-import com.alaaeltaweel.thikrallah.Notification.ThikrMediaPlayerService;
+import com.alaaeltaweel.thikrallah.ThikrMediaPlayerService;
 
 public class ThikrAlarmReceiver extends BroadcastReceiver {
     String TAG = "ThikrAlarmReceiver";
