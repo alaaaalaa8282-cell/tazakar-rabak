33dad1711729d18fbcf95f9be0fb8835a32ee9df
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-07-05 12:32:43 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- app/src/main/java/com/alaaeltaweel/thikrallah/Notification/ThikrService.java
+++ app/src/main/java/com/alaaeltaweel/thikrallah/Notification/ThikrService.java
@@ -243,6 +243,7 @@ sharedPrefs.edit().putInt("thikr_current_index", currentIndex + 1).apply();
             pendingThikrAfterCall = true;
         }
 			}
+            new MyAlarmsManager(getApplicationContext()).UpdateAllApplicableAlarms();
             return;
 
 		}
