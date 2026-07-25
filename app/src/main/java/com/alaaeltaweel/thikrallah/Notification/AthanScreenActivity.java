42f03f12c3da04315dae95f5991405d142677d53
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-07-25 13:18:58 +0300

    Refactor dua playback to use playDua method

--- app/src/main/java/com/alaaeltaweel/thikrallah/Notification/AthanScreenActivity.java
+++ app/src/main/java/com/alaaeltaweel/thikrallah/Notification/AthanScreenActivity.java
@@ -316,11 +316,8 @@ android.media.MediaPlayer mp = android.media.MediaPlayer.create(context, R.raw.d
         
         boolean isDuaEnabled = androidx.preference.PreferenceManager.getDefaultSharedPreferences(this).getBoolean("isDuaAfterAthan", false);
         if (isDuaEnabled) {
-            android.media.MediaPlayer mp = android.media.MediaPlayer.create(this, R.raw.dua_after_athan);
-            if (mp != null) {
-                mp.setOnCompletionListener(android.media.MediaPlayer::release);
-                mp.start();
-            }
+            playDua(this);
+        }
         }
         Intent stopThikr = new Intent(this, ThikrService.class);
         stopService(stopThikr);
